7853bf93b987a1f7752b9f3b5bd2194d02b2fe6f
gabyiy <gabyiy2000@hotmail.com>
2021-11-02 22:03:30 +0000

    next

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,23 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/all', methods=['GET'])
+def get_persons():
+    persons = Person.get_persons()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(persons), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods = ['GET'])
+def get_person_id(id):
+    person = Person.get_person_id(id)
+        
+    return jsonify(person), 200
+
+@app.route('/person', methods=['POST'])
+def create_person():
+    body = request.get_json()
+    Person.create_person(body['name'], body['last_name'], body['rol'], body['age'], body['father_id'], body['mother_id'])
+    return jsonify({"msg": "ok"}), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,58 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Person(db.Model):
+    __tablename__ = 'person'
+    id = db.Column(db.String(400), primary_key=True, nullable=True)
+    name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    rol = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    father_id = db.Column(db.String(400), db.ForeignKey ('person.id'), nullable= True)
+    mother_id = db.Column(db.String(400), db.ForeignKey ('person.id'), nullable = True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Person %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name,
+            "rol":self.rol,
+            "age":self.age,
+            "father_id":self.father_id,
+            "mother_id":self.mother_id
+                
             # do not serialize the password, its a security breach
         }
+
+    def get_persons():
+        persons = Person.query.order_by(Person.age.desc())
+        persons = list(map(lambda person: person.serialize(), persons))
+        return persons
+
+    def get_person_id(id):
+        Me = Person.query.get(id)
+        Father = Person.query.filter_by(id=Me.father_id).first()
+        if Father is None:
+            Father = []
+        Mother = Person.query.filter_by(id=Me.mother_id).first()
+        if Mother is None:
+            Mother = []
+        Child = Person.query.filter( (Person.father_id == id) | (Person.mother_id == id) ).first()
+        if Child is None:
+            Child = []
+        family = {
+            "father": Person.serialize(Father),
+            "mother": Person.serialize(Mother),
+            "me": Person.serialize(Me),
+            "child": Person.serialize(Child)
+            
+        }
+        
+   
+    def create_person(name, last_name, rol, age, father_id, mother_id):
+        person = Person(name=name, last_name=last_name, rol=rol, age=age, father_id=father_id, mother_id=mother_id)
+        db.session.add(person)
+        db.session.commit()
